039c56a918164883533d39c3c61095e612798928
Laís Dib Albuquerque <lda.snf20@uea.edu.br>
2022-02-06 23:57:54 -0400

    tutorial delete a post done

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,27 +1,36 @@
-import React from "react";
-import {Formik, Form, Field, ErrorMessage} from "formik";
+import React, { useContext, useEffect } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import axios from "axios";
 import { useHistory } from "react-router-dom";
+import { AuthContext } from "../helpers/AuthContext";
+
 
 function CreatePost() {
+  const { authState } = useContext(AuthContext);
   let history = useHistory();
   const initialValues = {
-      title: "",
-      postText: "",
-      username: "",
+    title: "",
+    postText: "",
   };
 
   const validationSchema = Yup.object().shape({
-      title: Yup.string().required(),
-      postText: Yup.string().required(),
-      username: Yup.string().min(3).max(15).required(),
+    title: Yup.string().required(),
+    postText: Yup.string().required(),
   });
 
+  useEffect(() => {
+    if (!localStorage.getItem("accessToken")) {
+      history.push("/login");
+    }
+  }, []);
+
   const onSubmit = (data) => {
-      axios.post("http://localhost:3001/posts", data).then((response) => {
-        history.push("/");
-      });
+    axios.post("http://localhost:3001/posts", data, {
+      headers: { accessToken: localStorage.getItem("accessToken") },
+    }).then((response) => {
+      history.push("/");
+    });
   };
 
   return (
@@ -32,29 +41,22 @@ function CreatePost() {
         validationSchema={validationSchema}
       >
         <Form className="formContainer">
-            <label>Title: </label>
-            <ErrorMessage name="Title" component="span"/>
-            <Field
-              id="inputCreatePost"
-              name="title"
-              placeholder="(Ex. Title...)"
-            />
-            <label>Post: </label>
-            <ErrorMessage name="Post Text" component="span"/>
-            <Field
-              id="inputCreatePost"
-              name="postText"
-              placeholder="(Ex. Post...)"
-            />
-            <label>Username: </label>
-            <ErrorMessage name="Username" component="span"/>
-            <Field
-              id="inputCreatePost"
-              name="username"
-              placeholder="(Ex. John123...)"
-            />
+          <label>Title: </label>
+          <ErrorMessage name="Title" component="span" />
+          <Field
+            id="inputCreatePost"
+            name="title"
+            placeholder="(Ex. Title...)"
+          />
+          <label>Post: </label>
+          <ErrorMessage name="Post Text" component="span" />
+          <Field
+            id="inputCreatePost"
+            name="postText"
+            placeholder="(Ex. Post...)"
+          />
 
-            <button type="submit"> Create Post </button>
+          <button type="submit"> Create Post </button>
         </Form>
       </Formik>
     </div>
